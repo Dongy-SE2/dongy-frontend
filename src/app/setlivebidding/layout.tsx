ac653e9eeab2686162cf9f74aac76f1f98dd1689
peemjunderscorej <peempeem153@gmail.com>
2025-03-08 20:54:58 +0700

    SetLiveBiddingCardComponent

---
+++ src/app/setlivebidding/layout.tsx
@@ -0,0 +1,12 @@
+interface Props {
+    children: React.ReactNode
+}
+
+export default function SetLiveBiddingLayout({ children }: Props){
+    return(
+        <>
+        { children }
+        </>
+    );
+}
+
---
+++ src/app/setlivebidding/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+import SetLiveBidding_LiveHistory from "@/components/SetLiveBidding_LiveHistory"
+import SetLiveBidding_LiveHistoryCard from "@/components/SetLiveBidding_LiveHistoryCard";
+
+export default function SetLiveBidding(){
+    return(
+        <>
+        <div>
+            <SetLiveBidding_LiveHistoryCard Title = {"First Live"}/>
+        </div>
+        </>
+    );
+}
