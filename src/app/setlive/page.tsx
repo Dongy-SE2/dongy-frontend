6122097cd04cc47ec19441f750b3d9900b1e513c
Book <ahtusaw@gmail.com>
2025-03-26 20:03:34 +0700

    fixed: refactor

--- src/app/setlive/page.tsx
+++ src/app/setlive/page.tsx
@@ -17,23 +17,25 @@ export default async function SetLiveBiddingPage() {
   const lives = await getLiveList(session.user.id, session.user.jwt);
   const products = await getProductList(session.user.jwt, session.user.id);
   return (
-    <ProductWraper>
-      <ProductManageHeader name="จัดการไลฟ์" href="/user" />
-      <div className="flex w-full justify-evenly mt-4">
-        <LiveContext>
-          <LiveList
-            lives={lives || []}
-            token={session.user.jwt}
-            sellerId={session.user.id}
-          />
-          <LiveManage
-            lives={lives || []}
-            token={session.user.jwt}
-            products={products}
-            sellerName={`${session.user.firstname} ${session.user.lastname}`}
-          />
-        </LiveContext>
-      </div>
-    </ProductWraper>
+    <div className="min-h-screen w-full flex justify-center bg-gradient-to-b from-emerald-100 from-0% via-slate-50 via-30% to-gray-50 to-100% p-16">
+      <ProductWraper>
+        <ProductManageHeader name="จัดการไลฟ์" href="/user" />
+        <div className="flex w-full justify-evenly mt-4">
+          <LiveContext>
+            <LiveList
+              lives={lives || []}
+              token={session.user.jwt}
+              sellerId={session.user.id}
+            />
+            <LiveManage
+              lives={lives || []}
+              token={session.user.jwt}
+              products={products}
+              sellerName={`${session.user.firstname} ${session.user.lastname}`}
+            />
+          </LiveContext>
+        </div>
+      </ProductWraper>
+    </div>
   );
 }
